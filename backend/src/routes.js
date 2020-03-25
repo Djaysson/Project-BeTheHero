f2f8d9f26f86637bb48778260e2e0331943f59a9
Djaysson <djayson.dev@gmail.com>
2020-03-25 11:34:06 -0300

    Creating the NGOs registry and listing

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,31 @@
 const express = require('express');
+const crypto = require('crypto');
+
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.post('/users', (request, response) => {
-  const body = request.body;
-  
-  console.log(body);
+routes.get('/ongs', async (request, response) =>{
+  const ongs = await connection('ongs').select('*');
 
-  return response.json({
-    evento: 'Semana Omnistack 11',
-    aluno: 'jayson Rodrigues'
-  });
+  return response.json(ongs);
+})
+
+routes.post('/ongs', async (request, response) => {
+  const {name,email,whatsapp,city,uf} = request.body;
+
+  const id = crypto.randomBytes(4).toString('Hex');
+
+  await connection('ongs').insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf,
+  })
+
+  return response.json({ id });
 });
 
 module.exports = routes;
