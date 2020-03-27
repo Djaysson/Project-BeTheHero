59d3a846d59d3ac41e256c82208c842d5fbca7ac
Djaysson <djayson.dev@gmail.com>
2020-03-27 18:29:31 -0300

    Incident screen and completed detail screen

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Routes from './src/routes';
+
 export default function App() {
   return (
-    <View>
-      <Text>Welcome to React native</Text>
-    </View>
+    <Routes />
   );
 }
 
